d6d645f2d51d1dd28eda77940db4311142960b39
Sushsnat <grtsushant@gmail.com>
2023-10-07 11:41:05 +0530

    added teams ans members functionality

--- App_Team_backend/src/index.js
+++ App_Team_backend/src/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 require('dotenv').config();
 const apiRoute=require("./routes")
 const EventRout = require("./routes/Events");
+const teamrouter = require("./routes/TeamRoutes")
 
 
 connect()
@@ -11,10 +12,12 @@ console.log("connected");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({extended:true}))
 
 app.use('/api',apiRoute)
 app.use("/event", EventRout);
+app.use("/team", teamrouter);
 
 const port = process.env.PORT;
 console.log(process.env.port);
---
+++ App_Team_backend/src/swagger/EventDoc.js
@@ -0,0 +1,166 @@
+/**
+ * @swagger
+ * components:
+ *   schemas: 
+ *      eventSchema:
+ *          type: object
+ *          properties:
+ *           name:
+ *            type: string
+ *            description: name of event
+ *           startDate:
+ *            type: string
+ *            description: start Date of event
+ *           endDate: 
+ *            type: string
+ *            description: end date of event
+ *           venue: 
+ *            type: array
+ *            description: venue of the event
+ *           info:
+ *              type: string
+ *              description: info about the event 
+ *           image:
+ *              type: string
+ *              description: url of the image
+ *           isWorkshop: 
+ *            type: Booelan
+ *           regUrl:
+ *            type: string
+ *            description: registration url 
+ */
+
+
+/**
+ * @swagger
+ * /getEvent:
+ *      get: 
+ *         tags: ['Events']
+ *         summary: Api to get all events
+ *         discription: Api to get all events
+ *         responses:
+ *            '200':
+ *               discription: all events
+ *               content:
+ *                  application/json:
+ *                     schema:
+ *            '500':
+ *                description: Error loading data
+ */
+
+
+/**
+ * @swagger
+ *  /postEvent:
+ *         post:
+ *            tags: ['Events']
+ *            summary: Api to  Post Events
+ *            requestBody:
+ *                  required: true
+ *                  content:
+ *                     application/json:
+ *                         schema:
+ *                      
+ *                             example:
+ *                                 name: HacknHills,
+ *                                 startDate: 2023-02-06T13:08:52,
+ *                                 endDate: 2023-03-10T00:36:08,
+ *                                 venue: OAT
+ *                                 regUrl: www.google.com
+ *                                 image: sdslfxdfgdgfhcffghgdf
+ *                                 info: Hacked hIlls
+ *                                 isWorkshop: true  
+ * 
+ *            responses:
+ *                '201':
+ *                   description: all users with "key" in usernames
+ *                   content:
+ *                       application/json:
+ *                           schema:
+ * 
+ *                '500':
+ *                  description: error posting event
+ */
+
+/**
+ * @swagger
+ *  /searchEvent?search=searchWord:
+ *      get:
+ *        tags: ['Events']
+ *        summary: Api to get searched events
+ *        discription: Api to get search events
+ *        parameters:
+ *           - in: query
+ *             name: search
+ *             schema:
+ *                type: text
+ *        responses:
+ *            '200':
+ *               discription: Events related to search
+ *               content:
+ *                  application/json:
+ *                     schema:
+ *            '500':
+ *                description: Error loading data    
+ */
+
+/**
+ * @swagger
+ *  /deleteEvent/:id:
+ *      delete:
+ *        tags: ['Events']
+ *        summary: Api to Delete event
+ *        discription: Api to Delete events
+ *        parameters:
+ *           - in: query
+ *             required: true
+ *             name: id
+ *             schema:
+ *                type: objectId
+ *        responses:
+ *            '200':
+ *               discription: Deleted event
+ *               content:
+ *                  application/json:
+ *                     schema:
+ *            '500':
+ *                description: INternal server Error   
+ */
+
+/**
+ * @swagger
+ *  /updateEvent/:id:
+ *      patch:
+ *        tags: ['Events']
+ *        summary: Api to Update event
+ *        requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
+ *                      schema:
+ * 
+ *                                example:
+ *                                 name: HacknHills,
+ *                                 startDate: 2023-02-06T13:08:52,
+ *                                 endDate: 2023-03-10T00:36:08,
+ *                                 venue: OAT
+ *                                 regUrl: www.google.com
+ *                                 image: sdslfxdfgdgfhcffghgdf
+ *                                 info: Hacked hIlls
+ *                                 isWorkshop: true 
+ *        parameters:
+ *            - in: query
+ *              required: true
+ *              name: id
+ *              schema:
+ *                  type: objectId
+ *        responses:
+ *            '200':
+ *               discription: Upd eveatednt
+ *               content:
+ *                  application/json:
+ *                     schema:
+ *            '500':
+ *                description: INternal server Error   
+ */
+
