1b36f2aff26eaf60b350ac6e59c155ff0b3f58db
karanpal97 <karangill1810@gmail.com>
2024-02-09 19:13:56 +0530

    added the dedicated to

--- src/models/confession.js
+++ src/models/confession.js
@@ -1,16 +1,18 @@
-const mongoose = require("mongoose")
-
+const mongoose = require("mongoose");
 
 const confessionSchema = new mongoose.Schema({
-      conf_message: {
-           type: String,
-      },
-      user:{
-         type: mongoose.Schema.Types.ObjectId,
-         ref: "Users2",
-      }
-   });
+    conf_message: {
+        type: String,
+    },
+    dedicated_to: {
+        type: String,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Users2",
+    },
+});
 
-   const Confession = mongoose.model("confession", confessionSchema);
+const Confession = mongoose.model("confession", confessionSchema);
 
-   module.exports = Confession;
+module.exports = Confession;
